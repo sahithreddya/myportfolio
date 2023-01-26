5437e06be2fddf1dbdcb12a5d3e742c3b8633784
Sahith <sahithreddy.a@gmail.com>
2023-01-26 17:31:34 -0600

    Added Beagle page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 import Mirkwood from './Webpages/Mirkwood';
 import Home from './Webpages/Home';
 import LinkApp from './Webpages/LinkApp';
+import Beagle from './Webpages/Beagle';
 
 const rootElement = document.getElementById("root");
 render(
@@ -16,6 +17,7 @@ render(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/LinkApp" element={<LinkApp />} />
+      <Route path="/Beagle" element={<Beagle />} />
       <Route path = "*" element = {<Mirkwood/>} />
     </Routes>
   </Router>,
