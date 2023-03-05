45fd28feaad731974475eff7895238a9497c2317
Sahith <sahithreddy.a@gmail.com>
2023-03-05 18:08:11 -0500

    EnggWeek Logo + Hyd Logo

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import LinkApp from './Webpages/LinkApp';
 import Beagle from './Webpages/Beagle';
 import AboutMe from './Webpages/AboutMe';
 import HydTT from './Webpages/HydTT';
+import HydLogo from './Webpages/HydLogo';
+import EnggWeekLogo from './Webpages/EnggWeekLogo';
 
 const rootElement = document.getElementById("root");
 render(
@@ -22,6 +24,8 @@ render(
       <Route path="/LinkApp" element={<LinkApp />} />
       <Route path="/Beagle" element={<Beagle />} />
       <Route path="/Hyderabad%20Tourism%20Website" element={<HydTT />} />
+      <Route path="/Tourism%20Brand%20Logo" element={<HydLogo />} />
+      <Route path="/Engineer's%20Week%20Logo" element={<EnggWeekLogo />} />
       <Route path = "*" element = {<Mirkwood/>} />
     </Routes>
   </Router>,
