86cf10550b0c74eea40dcd45064790f7452a5e9e
Sahith <sahithreddy.a@gmail.com>
2023-04-29 18:50:59 -0400

    Initializing GoogleAnalytics4 integration

--- src/Components/HeaderBar.js
+++ src/Components/HeaderBar.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import resume from '../Assets/Files/MyResume.pdf'
 import '../Styles/HeaderBar.css'
+import InitializeGoogleAnalytics, { TrackGoogleAnalyticsEvent } from '../GoogleAnalytics/googleanalytics4';
 
 const HeaderBar = React.forwardRef((props, ref) => {
     let location = useLocation();
+    useEffect(()=>{
+        InitializeGoogleAnalytics(); //initializing GA4
+      })
 
     return (
         <div className="HeaderBar">
@@ -13,7 +17,7 @@ const HeaderBar = React.forwardRef((props, ref) => {
                 <Link to="/" state={{from: location.pathname, to: 'home'}}><p className='header-item'>Home</p></Link>
                 <Link to="/" state={{from: location.pathname, to: 'work'}}><p className='header-item'>Work</p></Link>
                 <Link to="/About" state={{from: location.pathname}}><p className='header-item'>About Me</p></Link>
-                <p style={{cursor: "pointer"}} onClick={() => window.open(resume)} className='header-item'>Résumé</p>
+                <p style={{cursor: "pointer"}} onClick={() => { TrackGoogleAnalyticsEvent( "User Activity", "opened resume", window.location.pathname ); window.open(resume); }} className='header-item'>Résumé</p>
             </div>
         </div>
     );
